4ed9e7e6b567f44ac053b73eebbd1578fcade080
Jason Tsai <tez125@g.ncu.edu.tw>
2017-04-20 00:05:56 +0800

    Initial Commit

---
+++ TMDatabase/TMDatabase.h
@@ -0,0 +1,19 @@
+//
+//  TMDatabase.h
+//  TMDatabase
+//
+//  Created by Jason Tsai on 2017/4/20.
+//  Copyright © 2017年 YomStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TMDatabase.
+FOUNDATION_EXPORT double TMDatabaseVersionNumber;
+
+//! Project version string for TMDatabase.
+FOUNDATION_EXPORT const unsigned char TMDatabaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMDatabase/PublicHeader.h>
+
+
